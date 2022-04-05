a156f38af7bc51a9fdadb13860dc885b4122732d
Radoslav Georgiev <radorado@hacksoft.io>
2022-04-05 09:55:48 +0300

    Switch upload pages

--- src/pages/FilesUpload/index.js
+++ src/pages/FilesUpload/index.js
@@ -1,12 +1,14 @@
 // See FilesUploadJsExample file if you want to have this code in pure html
 
+import DirectUploadExample from './Direct';
 import FileUploadReactExample from './FileUploadReactExample';
 
 const UploadFiles = () => {
   return (
-    <>
-      <FileUploadReactExample />
-    </>
+    <div>
+      {false && <DirectUploadExample />}
+      {true && <FileUploadReactExample />}
+    </div>
   );
 };
 
